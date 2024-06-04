a3aac2675a7af64b147d54b9a3721f697f09fc22
Yusuf Umar <yusufumaris@gmail.com>
2024-06-04 21:08:54 +0700

    Remove any auto update codes for blender extension platform

--- __init__.py
+++ __init__.py
@@ -45,14 +45,12 @@ if "bpy" in locals():
     imp.reload(BakeToLayer)
     imp.reload(Root)
     imp.reload(versioning)
-    imp.reload(addon_updater_ops)
     imp.reload(Test)
 else:
     from . import Localization
     from . import BaseOperator, image_ops, common, bake_common, modifier_common, lib, ui, subtree, transition_common, input_outputs, node_arrangements, node_connections, preferences
     from . import vector_displacement_lib, vector_displacement
     from . import vcol_editor, transition, BakeTarget, BakeInfo, UDIM, ImageAtlas, MaskModifier, Mask, Modifier, NormalMapModifier, Layer, ListItem, Bake, BakeToLayer, Root, versioning
-    from . import addon_updater_ops
     from . import Test
 
 import bpy 
@@ -81,7 +79,6 @@ def register():
     BakeToLayer.register()
     Root.register()
     versioning.register()
-    addon_updater_ops.register()
     Test.register()
 
     print('INFO: ' + common.get_addon_title() + ' ' + common.get_current_version_str() + ' is registered!')
@@ -110,7 +107,6 @@ def unregister():
     BakeToLayer.unregister()
     Root.unregister()
     versioning.unregister()
-    addon_updater_ops.unregister()
     Test.unregister()
 
     print('INFO: ' + common.get_addon_title() + ' ' + common.get_current_version_str() + ' is unregistered!')
